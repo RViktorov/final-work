70715397741cfe4bfacd7cbb4ee11c8fc16133c3
Viktorov Roman <rpviktorov@rambler.ru>
2022-09-17 22:42:04 +0300

    solution task

---
+++ solution/Program.cs
@@ -0,0 +1,19 @@
+﻿String[] matrix = { "привет", "пока", "ого", ";-)", ":-(", "ель", "собака", "12443" };
+int sizeNewMatrix = 0;
+for (int i = 0; i < matrix.Length; i++)
+{
+    {
+        if (matrix[i].Length <= 3) { sizeNewMatrix++; }
+    }
+}
+String[] newMatrix = new String[sizeNewMatrix];
+int j = 0;
+for (int i = 0; i < matrix.Length; i++)
+{
+    if (matrix[i].Length <= 3)
+    {
+        newMatrix[j] = matrix[i];
+        j++;
+    }
+}
+Console.WriteLine("[{0}]", string.Join(", ", newMatrix));
